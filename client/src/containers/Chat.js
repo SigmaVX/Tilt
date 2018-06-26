b6b2dbc9b53b541975117248e74513dbb3a86169
SigmaVX <tonywible@gmail.com>
2018-06-26 00:18:43 -0400

    adding initial front end files

---
+++ client/src/containers/Chat.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Chat = (props) => {
+
+    return (
+
+        <div className="row no-gutters jumbotron text-center">
+            <h1 className="col-12 animated pulse" >Chat</h1>
+            <h2 className="col-12">Add Some Text</h2>
+            <h3 className="col-12">Add More Text</h3>
+        </div>
+    )
+} 
+
+export default Chat;
