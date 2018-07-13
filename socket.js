2362e99586741f6f2f1db1d511948bd9235f2030
Fabian Flores <ffflores1@outlook.com>
2018-07-13 17:46:57 -0400

    restructured code and dry'd up sockets and listeners

--- socket.js
+++ socket.js
@@ -3,54 +3,55 @@ module.exports = function (io) {
   io.on("connection", function (socket) {
     var joinedUser = false;
 
-    socket.on("General", function (data) {
-      console.log(`General: ${data.msg}`);
-      io.emit("General", data);
-    });
-    socket.on("League of Legends", function (data) {
-      console.log(`League of Legends: ${data.msg}`);
-      io.emit("League of Legends", data);
-    });
-    socket.on("Overwatch", function (data) {
-      console.log(`Overwatch: ${data.msg}`);
-      io.emit("Overwatch", data);
-    });
-    socket.on("Fortnite", function (data) {
-      console.log(`Fortnite: ${data.msg}`);
-      io.emit("Fortnite", data);
-    });
-    socket.on("Destiny", function (data) {
-      console.log(`Destiny: ${data.msg}`);
-      io.emit("Destiny", data);
-    });
-    socket.on("Anthem", function (data) {
-      console.log(`Anthem: ${data.msg}`);
-      io.emit("Anthem", data);
-    });
-    socket.on("PUBG", function (data) {
-      console.log(`PUBG: ${data.msg}`);
-      io.emit("PUBG", data);
-    });
-    socket.on("Call of Duty", function (data) {
-      console.log(`Call of Duty: ${data.msg}`);
-      io.emit("Call of Duty", data);
-    });
-    socket.on("World of Warcraft", function (data) {
-      console.log(`World of Warcraft: ${data.msg}`);
-      io.emit("World of Warcraft", data);
-    });
+    socket
+      .on("General", function (data) {
+        console.log(`General: ${data.msg}`);
+        io.emit("General", data);
+      })
+      .on("League of Legends", function (data) {
+        console.log(`League of Legends: ${data.msg}`);
+        io.emit("League of Legends", data);
+      })
+      .on("Overwatch", function (data) {
+        console.log(`Overwatch: ${data.msg}`);
+        io.emit("Overwatch", data);
+      })
+      .on("Fortnite", function (data) {
+        console.log(`Fortnite: ${data.msg}`);
+        socket.emit("Fortnite", data);
+      })
+      .on("Destiny", function (data) {
+        console.log(`Destiny: ${data.msg}`);
+        socket.emit("Destiny", data);
+      })
+      .on("Anthem", function (data) {
+        console.log(`Anthem: ${data.msg}`);
+        socket.emit("Anthem", data);
+      })
+      .on("PUBG", function (data) {
+        console.log(`PUBG: ${data.msg}`);
+        socket.emit("PUBG", data);
+      })
+      .on("Call of Duty", function (data) {
+        console.log(`Call of Duty: ${data.msg}`);
+        socket.emit("Call of Duty", data);
+      })
+      .on("World of Warcraft", function (data) {
+        console.log(`World of Warcraft: ${data.msg}`);
+        socket.emit("World of Warcraft", data);
+      });
 
     socket.on("add user", function(data) {
       console.log(`${data.uname} joined ${data.room} chatroom. msg is ${data.msg}`);
       if (joinedUser) {
         numUsers++;
       }
-      io.emit(data.room, data);
+      socket.emit(data.room, data);
     });
-    socket.on("user state", function (msg) {
+/*     socket.on("user state", function (msg) {
       console.log(`user state message: ${msg}`);
       io.emit("user state", msg);
-    });
+    }); */
     socket.on("disconnect", function () {
       console.log("user disconnected");
       numUsers--;
@@ -59,7 +60,7 @@ module.exports = function (io) {
       console.log(`${data.uname} left ${data.room} chatroom. msg is ${data.msg}`);
       joinedUser = false;
       numUsers--;
-      io.emit(data.room, data);
+      socket.emit(data.room, data);
     })
   });
 }
