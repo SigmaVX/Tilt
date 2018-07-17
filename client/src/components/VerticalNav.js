be32b6f0dce55cddb7f47959d6194f7c8fe83632
SigmaVX <tonywible@gmail.com>
2018-07-17 18:55:12 -0400

    added new nav style

--- client/src/components/VerticalNav.js
+++ client/src/components/VerticalNav.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
-import { slide as Menu } from 'react-burger-menu';
+import { bubble as Menu } from 'react-burger-menu';
 
 
 function AdminBar(props) {
